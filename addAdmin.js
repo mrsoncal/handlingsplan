d2ae7075d8a029e2ed4c95ff2b5fad5ac2856a68
mrsoncal <mrsoncal@gmail.com>
2025-07-03 13:45:26 +0200

    Update addAdmin.js

--- addAdmin.js
+++ addAdmin.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 
 const db = new sqlite3.Database('./data.db');
 const username = 'admin';
-const password = 'admin123';
+const password = 'password123';
 
 bcrypt.hash(password, 10, (err, hash) => {
   db.run('INSERT INTO admins (username, password) VALUES (?, ?)', [username, hash], () => {
