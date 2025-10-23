3110475f270e1259e26902db4a6a48c73ed6953a
mrsoncal <mrsoncal@gmail.com>
2025-10-23 20:13:57 +0200

    -

--- talestolen-src/vite.config.js
+++ talestolen-src/vite.config.js
@@ -1,8 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/talestolen-src',         // so assets resolve correctly from subpath
-  build: { outDir: '../talestolen-src', emptyOutDir: true }
-})
+export default { };
+  ,
+  base: '/talestolen/',
+  build: { outDir: '../talestolen' }
+}
