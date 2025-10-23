f37cff1cf8af030d75cc5508dfd3d7f50e6404b2
mrsoncal <mrsoncal@gmail.com>
2025-10-23 20:20:13 +0200

    -

--- talestolen-src/vite.config.js
+++ talestolen-src/vite.config.js
@@ -1,5 +1,9 @@
-export default { };
-  ,
-  base: '/talestolen/',
-  build: { outDir: '../talestolen' }
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Build from /talestolen-src into ../talestolen (static, production-ready)
+export default defineConfig({
+  plugins: [react()],
+  base: '/talestolen/',                  // app will live under /talestolen/
+  build: { outDir: '../talestolen', emptyOutDir: true }
+})
