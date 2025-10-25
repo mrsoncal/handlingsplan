9387019f768a7f6d17220e24b1387ed24e6bc610
mrsoncal <mrsoncal@gmail.com>
2025-10-25 14:22:16 +0200

    -

--- talestolen-src/src/App.jsx
+++ talestolen-src/src/App.jsx
@@ -59,6 +59,7 @@ function useTimerRerender(enabled) {
 export default function App() {
   const state = useStore();
   const hash = useHash();
+  useTimerRerender(hash !== "#timer");
   useTimerRerender(hash !== "#queue");
 
   if (hash === "#timer") return <TimerFull state={state} />;
